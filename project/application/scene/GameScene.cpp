3c7ebc6803e41808fe19a1ef0752fa6f5d3b496d
miyanao <k023g0050@g.neec.ac.jp>
2025-06-18 19:50:15 +0900

    不要箇所の削除

--- project/application/scene/GameScene.cpp
+++ project/application/scene/GameScene.cpp
@@ -41,15 +41,7 @@ void GameScene::Initialize()
 
 #pragma region パーティクル
 	// パーティクル
-	particleHitEmitter_.reset(new ParticleEmitter);
-	particleHitEmitter_->Initialize("hitEffect", "resources/texture/circle.png", ParticleEmitter::Box);
-	particleHitEmitter_->SetPosition({ 0.0f, 1.0f, 0.0f });
-	particleRingEmitter_.reset(new ParticleEmitter);
-	particleRingEmitter_->Initialize("Ring", "resources/texture/gradationLine.png", ParticleEmitter::Ring);
-	particleRingEmitter_->SetPosition({ 0.0f, 1.0f, 0.0f });
-	particleCylinderEmitter_.reset(new ParticleEmitter);
-	particleCylinderEmitter_->Initialize("Cylinder", "resources/texture/gradationLine.png", ParticleEmitter::Cylinder);
-	particleCylinderEmitter_->SetPosition({ 0.0f, 0.0f, 0.0f });
+	
 #pragma endregion パーティクル
 
 #pragma region オーディオ
@@ -331,9 +323,7 @@ void GameScene::Update()
 	
 	// パーティクル
 	ParticleManager::GetInstance()->Imgui();
-	particleHitEmitter_->Imgui("Player");
-	particleRingEmitter_->Imgui("Enemy");
-	particleCylinderEmitter_->Imgui("Magic");
+	
 
 	ImGui::Text("\n");
 
